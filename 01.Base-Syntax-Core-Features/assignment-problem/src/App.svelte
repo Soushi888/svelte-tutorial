059fcfffc7d4eca8170c8188dffd7c5f1e388c43
soushi888 <sacha.pignot@protonmail.com>
2021-08-21 03:41:16 -0400

    first assignment finished

--- 01.Base-Syntax-Core-Features/assignment-problem/src/App.svelte
+++ 01.Base-Syntax-Core-Features/assignment-problem/src/App.svelte
@@ -1,16 +1,10 @@
-<script></script>
+<script>
+  import CourseGoal from "./CourseGoal.svelte";
 
-<h1>Assignment</h1>
+  let courseGoal = "";
+</script>
 
-<p>Solve these tasks.</p>
 
-<ol>
-  <li>Add an input field that allows users to enter a course goal.</li>
-  <li>Output the user input in a h1 tag.</li>
-  <li>
-    Color the output red (e.g. by adding a class) if it contains at least one exclamation mark.
-  </li>
-  <li>
-    Put the h1 tag + output into a separate component to which you pass the user input
-  </li>
-</ol>
+<label for="course-goal">Course Goal : </label><input type="text" id="course-goal" bind:value={courseGoal}>
+
+<CourseGoal {courseGoal}/>
---
+++ 01.Base-Syntax-Core-Features/assignment-problem/src/CourseGoal.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let courseGoal;
+
+  $: hasExclamation = courseGoal.includes('!');
+</script>
+
+<h1 class:exclamation={hasExclamation}>{courseGoal}</h1>
+
+<style>
+    .exclamation {
+        color: red;
+    }
+</style>
