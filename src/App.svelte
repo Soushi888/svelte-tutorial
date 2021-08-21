fd73143769244b646022a81241bafc2ca83d16cc
soushi888 <sacha.pignot@protonmail.com>
2021-08-21 03:02:37 -0400

    adding dynamic props to ContactCard

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,28 @@
 <script>
   import ContactCard from "./ContactCard.svelte";
 
-  let age = 28;
-  let name = "Sacha"https://embed-ssl.wistia.com/deliveries/1728b1fca23dc72cf736828c0da20279ddb05525.jpg?image_crop_resized
-
-  $: uppercaseName = name.toUpperCase();
-  $: console.log(name);
-
-  const incrementAge = () => age++;
+  let image = "https://avatars.githubusercontent.com/u/29674937?s=400&u=b6c37ee87cc8ed4d5d1205cf8ad53b5f33cea6df&v=4";
+  let userName = "Sacha";
+  let jobTitle = "Software architect"
+  let description = "I am specialized in distributed architectures and collective intelligence.";
 </script>
+
+
+<label for="image">Image : </label><input id="image" type="text" bind:value={image}><br>
+<label for="name">Name : </label><input id="name" type="text" bind:value={userName}><br>
+<label for="jobTitle">Job Title : </label><input id="jobTitle" type="text" bind:value={jobTitle}><br>
+<label for="description">Description : </label><textarea name="description" id="description" cols="50" rows="3"
+                                                         bind:value={description}></textarea>
+
+<ContactCard {userName} {jobTitle} {image} {description}/>
+
 
 <style>
     h1 {
         color: purple;
     }
+
+    #image {
+        width: 500px;
+    }
 </style>
-
-<h1>Hello {uppercaseName} ! You age is {age} !</h1>
-<button on:click={incrementAge}>Happy birthday !</button>
-
-<input type="text" bind:value={name}>
-
-<ContactCard name="{name}"/>
