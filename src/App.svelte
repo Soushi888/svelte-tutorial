94ce560e83b96a7f38bd8458886d1f8c72d90cb0
soushi888 <sacha.pignot@protonmail.com>
2021-08-21 01:28:44 -0400

    update App component and add ContactCard component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,24 @@
 <script>
-	export let name;
+  import ContactCard from "./ContactCard.svelte";
+
+  let age = 28;
+  let name = "Sacha"https://embed-ssl.wistia.com/deliveries/1728b1fca23dc72cf736828c0da20279ddb05525.jpg?image_crop_resized
+
+  $: uppercaseName = name.toUpperCase();
+  $: console.log(name);
+
+  const incrementAge = () => age++;
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+    h1 {
+        color: purple;
+    }
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Hello {uppercaseName} ! You age is {age} !</h1>
+<button on:click={incrementAge}>Happy birthday !</button>
+
+<input type="text" bind:value={name}>
+
+<ContactCard name="{name}"/>
