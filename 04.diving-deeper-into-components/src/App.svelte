6060e269d3200b9d3e6293c306e5368d6d794e4b
soushi888 <sacha.pignot@protonmail.com>
2021-09-01 00:51:04 -0400

    custom events + slot

--- 04.diving-deeper-into-components/src/App.svelte
+++ 04.diving-deeper-into-components/src/App.svelte
@@ -1,5 +1,39 @@
 <script>
-  import Product from "./Product.svelte"
+  import Product from "./Product.svelte";
+  import Modal from "./Modal.svelte";
+
+  let products = [
+    {
+      id: "p1",
+      title: "A book",
+      price: 9.99,
+      bestseller: true,
+    },
+    {
+      id: "p2",
+      title: "A pencil",
+      price: 2.99,
+    },
+  ];
+
+  function addToCart(evt) {
+    console.log(evt.type, evt.detail);
+  }
+
+  function deleteProduct(evt) {
+    console.log(evt.type, evt.detail);
+  }
 </script>
 
-<Product productTitle="Saxophone"/>
+{#each products as product (product.id)}
+  <Product
+    {...product}
+    on:add-to-cart={addToCart}
+    on:delete-product={deleteProduct}
+  />
+{/each}
+
+<Modal>
+  <h1>Hello World !</h1>
+  <p>Here I am !</p>
+</Modal>
--- 04.diving-deeper-into-components/src/Product.svelte
+++ 04.diving-deeper-into-components/src/Product.svelte
@@ -1,9 +1,25 @@
 <script>
-  export let productTitle;
+  import { createEventDispatcher } from "svelte";
+
+  export let title, price, id, bestseller = false;
+
+  const dispatch = createEventDispatcher();
+
+  function addToCart() {
+    dispatch("add-to-cart", { title, price, id });
+  }
+
+  function deleteProduct() {
+    dispatch("delete-product", { title, price, id });
+  }
 </script>
 
 <article>
-  <h1>{productTitle}</h1>
-  <button>Add to cart</button>
-  <button>Delete</button>
+  <h1>{title}</h1>
+  <h2>{price}$</h2>
+  {#if bestseller}
+    <h3>{"Bestseller".toUpperCase()}</h3>
+  {/if}
+  <button on:click={addToCart}>Add to cart</button>
+  <button on:click={deleteProduct}>Delete</button>
 </article>
