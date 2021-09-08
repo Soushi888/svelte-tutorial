b31154804e39ae89310892a639fbf60173ef9c73
soushi888 <sacha.pignot@protonmail.com>
2021-09-08 16:48:14 -0400

    state and data with stores

---
+++ 06.state-and-data-with-stores/src/Products/Products.svelte
@@ -0,0 +1,30 @@
+<script>
+  import productsStore from "../stores/products-store";
+  import Product from "../Products/Product.svelte";
+
+  let products;
+
+  productsStore.subscribe((i) => {
+    products = i;
+  });
+</script>
+
+<section>
+  <h1>Products</h1>
+  {#each products as product (product.id)}
+    <Product
+      id={product.id}
+      title={product.title}
+      price={product.price}
+      description={product.description}
+    />
+  {/each}
+</section>
+
+<style>
+  section {
+    width: 30rem;
+    max-width: 90%;
+    margin: 2rem auto;
+  }
+</style>
